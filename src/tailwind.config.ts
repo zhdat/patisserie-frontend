b4142145b4b440319b2a8df0ad3cd414949d2d11
calliste <calliste.ravix@capgemini.com>
2026-05-20 23:09:19 +0200

    feat: tailwind

---
+++ src/tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+        serif: ['"Playfair Display"', 'serif'],
+      },
+    },
+  },
+};
